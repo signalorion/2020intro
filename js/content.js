e80ba17fbc4589a5c830063c2c2822de6fb4a340
signalorion <masa4@daum.net>
2020-10-27 23:46:28 +0900

    컨텐츠페이지1 구성

---
+++ js/content.js
@@ -0,0 +1,118 @@
+//전역변수////////////////
+//1. 페이지번호를 먹이는 전역변수
+var pno = 0;
+//2. 전체 페이지 수
+var totnum = 2;
+//3. 광스크롤방지
+var psts = 0; //0은 허용이고 1은 불허용이다
+//4. 한페이지 높이값
+var winH = $(window).height();
+
+$(function () {
+    /*전체페이지 스크롤*/
+    $(document).on("mousewheel DOMMouseScroll", function (e) {
+
+        /////// 광스크롤막기 /////////////////
+        if (psts === 1) return true; //돌아가!
+        psts = 1; //잠금(기존0값을 변경)
+        setTimeout(function () {
+            psts = 0;
+        }, 600); /// 타임아웃 ///
+        ////////////////////////////////////
+
+        e = window.event || e;
+        var delta = e.detail ? e.detail : e.wheelDelta;
+        //console.log("델타:"+delta);
+        //아랫방향
+        if (delta < 0) {
+            pno++;
+            if (pno === totnum) pno = totnum - 1;
+        } else {
+            pno--;
+            if (pno === -1) pno = 0;
+        }
+        //console.log("pno:" + pno);
+
+        $("html,body").stop().animate({
+            scrollTop: (winH * pno) + "px"
+        }, 400);
+
+    }); /////// mousewheel /////////////////
+
+    /*썸네일 호버시*/
+    $(".renew_hover1").hide();
+    $(".cont1_main section").hover(function () {
+        $(".renew_hover1", this).stop().fadeIn();
+    }, function () {
+        $(".renew_hover1", this).stop().fadeOut();
+    });
+
+    $(".cont1_main section a").click(function (e) {
+        e.preventDefault();
+    });
+
+    /*썸네일클릭시*/
+    $(".cont_detail").hide();
+    $(".renew_sec1").click(function () {
+        $(".cont_detail.cd1").fadeIn();
+    });
+    $(".renew_sec2").click(function () {
+        $(".cont_detail.cd2").fadeIn();
+    });
+    $(".renew_sec3").click(function () {
+        $(".cont_detail.cd5").fadeIn();
+    });
+
+    /*디테일페이지*/
+    /*디테일 썸네일클릭시*/
+    $(".imgtop").click(function () {
+        $(".imgtop", this).children().eq(0).hide();
+        $(".dnimg", this).fadeIn(400);
+    });
+    $(".imgbottom").click(function () {
+        $(".imgbottom", this).children().eq(0).hide();
+        $(".dnimg", this).fadeIn(400);
+    });
+    
+    /*5번*/
+    $(".img_left").click(function () {
+        $(".img_left").children().eq(0).hide();
+        $(".dnimg", this).fadeIn(400);
+    });
+    $(".img_right").click(function () {
+        $(".img_right").children().eq(0).hide();
+        $(".dnimg", this).fadeIn(400);
+    });
+    
+    
+    
+
+    $(".search_tit").click(function () {
+        $(".cont_detail").fadeOut(400);
+    });
+
+    $(".clicktoclose").click(function () {
+        $(".cont_detail").fadeOut(400);
+    });
+
+
+    $(".prev_detail").click(function () {
+        $(this).parent().hide();
+        $(this).parent().prev().show();
+    }); //////////이전클릭////////////////////
+
+    $(".next_detail").click(function () {
+        $(this).parent().hide();
+        $(this).parent().next().show();
+    }); //////////다음클릭////////////////////
+
+
+
+
+
+
+
+
+
+}); //////////제이쿼리코드블록////////////////////////////
+//////////////////////////////////////////////////////
--- js/js.js
+++ js/js.js
@@ -4,27 +4,26 @@
 var dcsb = 0; //0은 불허용이고 1은 허용
 
 
-
 $(function () {
-    /*인트로에서*/
+    /****************************************************인트로에서*/
     $(".search_box_fake").click(function () {
-        if(dcsb ===1){
+        if (dcsb === 1) {
             return false;
         }
         $(".clickhand").hide();
         $(".search_intro_box").animate({
             top: "0"
-        }, 600);//////인트로박스 위치설정/////////
+        }, 600); //////인트로박스 위치설정/////////
         $(".search_intro_box").css({
             transform: "translateX(-50%)"
-        });/////////인트로박스 css설정////////
+        }); /////////인트로박스 css설정////////
         $(".bigsemo").fadeOut(200);
-        
+
         $(".intro_bbg").delay(900).slideDown();
         $(".searchtab").show();
-        
-        dcsb =1; //검색창이 열린 상태에서 검색박스를 눌렀을 때 x버튼을 누른 효과를 낼 순 없을까??
-        
+
+        dcsb = 1; //검색창이 열린 상태에서 검색박스를 눌렀을 때 x버튼을 누른 효과를 낼 순 없을까??
+
     });
     $(".clickhand, .bigsemo").click(function () {
         $(this).hide();
@@ -50,54 +49,55 @@ $(function () {
         $(".bigsemo").delay(300).fadeIn();
         $(".intro_bbg").hide();
         $(".searchtab").hide();
-        
-        dcsb=0;
+
+        dcsb = 0;
     });
 
-    $(".search_box").click(function(e){
+    $(".search_box").click(function (e) {
         e.preventDefault();
     });
-    
+
     /*개별 페이지공통*/
-    $(".search_box").click(function(){
+    $(".search_box").click(function () {
         $(".search_list").stop().slideToggle();
     });
-    
-    $(".sl_index_close").click(function(e){
+
+    $(".sl_index_close").click(function (e) {
         e.preventDefault();
         $(".search_list").slideUp();
     });
-    
-    $(".sl_index a").hover(function(){
+
+    $(".sl_index a").hover(function () {
         var sl_index = $(this).attr("data-color");
         //console.log(sl_index);
-        
+
         $(this).css({
-            backgroundColor:sl_index
+            backgroundColor: sl_index
         });
-    }, function(){
-        
+    }, function () {
+
         $(this).css({
-            background:"transparent"
+            background: "transparent"
         });
     });
-    
+
     /*내 소개 페이지*/
     //내가 이 페이지에 도달했을 때 이 효과가 나게 할 수 없을까?
-   $(".mycertifi, .mystudy, .mytool").animate({
-        width:"35%"
-    },700);
+    $(".mycertifi, .mystudy, .mytool").animate({
+        width: "35%"
+    }, 700);
+
+
+
+    /***************************************컨텐츠페이지*/
     
 
-    
 
-   
-   
 
-    
-    
-    
-    
-    
+
+
+
+
+
 }); //////////제이쿼리코드블록////////////////////////////
 //////////////////////////////////////////////////////
